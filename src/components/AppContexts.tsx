f36938a29a209e2fe6259df72691b13520cbe82e
sedlakp <37884964+sedlakp@users.noreply.github.com>
2023-08-06 20:10:16 +0200

    Updates

--- src/components/AppContexts.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react"
-
-export enum AppTheme {
-    Light = "light",Dark = "dark"
-}
-
-export const ThemeContext = createContext({theme: AppTheme.Light, setTheme: (_: AppTheme) => {} })
